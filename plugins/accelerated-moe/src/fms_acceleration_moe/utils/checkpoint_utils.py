11fd54f3c19de47a83cc0b6d10af731c38a6eb75
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-08-18 01:24:12 +0530

    feat: dtensor grad norm
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- plugins/accelerated-moe/src/fms_acceleration_moe/utils/checkpoint_utils.py
+++ plugins/accelerated-moe/src/fms_acceleration_moe/utils/checkpoint_utils.py
@@ -632,8 +632,11 @@ def clip_grad_norm_(self, parameters, max_norm, norm_type=2):
     for p in parameters:
         if p.grad is None:
             continue
-        if p.device_mesh is None:
-            print(p)
+        print("*"*10)
+        print(p)
+        print(p.device_mesh)
+        print(p.device_mesh.mesh_dim_names)
+        print("*"*10)
         if KEY_EXPERT_PARALLEL in p.device_mesh.mesh_dim_names:
             ep_params.append(p)
             ep_grads.append(p.grad)
