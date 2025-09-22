149d1c0ea11d61407397de2ba8b529f7026b336c
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-09-22 22:55:58 +0530

    feat: add odm plugin
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- plugins/online-data-mixing/src/fms_acceleration_odm/patch.py
+++ plugins/online-data-mixing/src/fms_acceleration_odm/patch.py
@@ -14,7 +14,7 @@ def patch_fms_hf_tuning_data_utils_for_odm():
     # pylint: disable=import-outside-toplevel
     from fms_acceleration.model_patcher import patch_target_module
 
-    patch_target_module("tuning.data.data_processors._process_dataset_configs", _process_dataset_configs)
+    patch_target_module("tuning.data.data_processors.DataPreProcessor._process_dataset_configs", _process_dataset_configs)
     patch_target_module("tuning.data.setup_dataprocessor.process_dataargs", process_dataargs)
 
 def _process_dataset_configs(
