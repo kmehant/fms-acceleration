ef01cffea5768c80675fee684b76040782c7f088
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-09-23 23:03:08 +0530

    feat: add odm plugin
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- plugins/online-data-mixing/src/fms_acceleration_odm/odm/dataloader.py
+++ plugins/online-data-mixing/src/fms_acceleration_odm/odm/dataloader.py
@@ -147,7 +147,7 @@ class OnlineData(IterableDataset):
         eval_dataset_dict = {}
         self._reset_eval_dataloaders()
         for c in range(self.total_categories):
-            eval_dataset_dict[self.id2cat[c]] = accelerator.prepare(self.eval_dataset_dict[self.id2cat[c]])
+            eval_dataset_dict[self.id2cat[c]] = accelerator.prepare(self.eval_dataset_dict_dl[self.id2cat[c]])
         for c in range(self.total_categories):
             for batch in eval_dataset_dict[self.id2cat[c]]:
                 rewards[c] += compute_reward(model=model, batch={k: v.to(accelerator.device) for k, v in batch.items()}, vocab_size=32000, reward_type=self.reward_type, train_loop_metrics=metrics)
