093aad9079c82c6077a47ae42b70c3c95501a899
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-09-25 03:12:06 +0530

    feat: add odm_plugin
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- plugins/online-data-mixing/src/fms_acceleration_odm/odm/reward.py
+++ plugins/online-data-mixing/src/fms_acceleration_odm/odm/reward.py
@@ -108,6 +108,8 @@ def compute_reward(
         if reward_type == Reward.ENTROPY_LAST_TOKEN:
             return entropy_last_token.sum().item()
     if reward_type == Reward.TRAIN_LOSS:
+        if not train_loss_history:
+            raise ValueError("train_loss_history cannot be a empty list or None")
         if not TRAIN_LOSS_DATA["buffer"]:
             TRAIN_LOSS_DATA["buffer"] = [1e-100] * total_categories
         TRAIN_LOSS_DATA["buffer"][last_sampled_category] = train_loss_history[-1][
@@ -115,11 +117,18 @@ def compute_reward(
         ]
         return TRAIN_LOSS_DATA["buffer"][current_category]
     if reward_type == Reward.VALIDATION_LOSS:
+        if not eval_loss_history:
+            raise ValueError("eval_loss_history cannot be a empty list or None."
+                             "Make sure you are using eval_strategy and eval_steps"
+                             "allowing atleast 1 evaluation before reward computation.")
         if not EVAL_LOSS_DATA["buffer"]:
             EVAL_LOSS_DATA["buffer"] = [1e-100] * total_categories
         EVAL_LOSS_DATA["buffer"][current_category] = eval_loss_history[-1]["loss"]
         return EVAL_LOSS_DATA["buffer"][current_category]
     if reward_type == Reward.GRADNORM:
+        if not gradnorm_history:
+            raise ValueError("gradnorm_history cannot be a empty list or None."
+                             "Make sure grad norm is made available.")
         if not GRADNORM_DATA["buffer"]:
             GRADNORM_DATA["buffer"] = [1e-100] * total_categories
         GRADNORM_DATA["buffer"][last_sampled_category] = 1 / (
